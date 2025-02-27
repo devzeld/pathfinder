b42d762de0b8d45b8a1b44b00ef5e112c9e60fde
eldis.izeirovski <zeld.exe.dev@gmail.com>
2025-02-27 13:27:03 +0100

    first commit

---
+++ js/Obstacle.js
@@ -0,0 +1,13 @@
+class Obstacle {
+    constructor(x, y, width, height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    isUnder(xState, yState) {
+        return (xState >= this.x && xState <= this.x + this.width) &&
+            (yState >= this.y && yState <= this.y + this.height);
+    }
+}
